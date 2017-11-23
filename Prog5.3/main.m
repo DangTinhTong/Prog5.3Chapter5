8f56d78ea3bbef2fbd9653b0745454ecf89527c8
admin <dangtinhtong@gmail.com>
2017-11-23 16:42:11 +0700

    first commit

--- Prog5.3/main.m
+++ Prog5.3/main.m
@@ -2,16 +2,29 @@
 //  main.m
 //  Prog5.3
 //
-//  Created by admin on 11/23/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/23/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
+//-------------------------
+// program to generate a table of triangular numbers
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+//        NSLog(@"Hello, World!");
+  
+        int n, triangulator;
+        NSLog(@"The table of triangular numbers");
+        NSLog(@"n sum from 1 to n");
+        NSLog(@"------------------");
+        triangulator =0;
+        for (n=1; n<=10; ++n) {
+            triangulator+=n;
+            NSLog(@" % i          % i",n, triangulator);
+        }
+    
     }
     return 0;
 }
